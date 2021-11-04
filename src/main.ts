c0af5ffed6273de4f2c8b07077bef39ef6ce5153
Jason <exanix@gmail.com>
2021-11-04 15:48:37 -0700

    feat(main): use custom `git log` command within node process
    This allows us to customize how the log output is formatted
    for more predictable parsing.

--- src/main.ts
+++ src/main.ts
@@ -5,17 +5,26 @@ import { getPrettyLog } from "./pretty_parser";
 import { writeFileSync } from "fs";
 import config from '../config.json';
 import { pipe } from "ramda";
+import { exec } from "child_process";
+import chalk from "chalk";
 
 
 const title = process.argv[2] ?? 'untitled';
 
-
-pipe(
-  () => getLogsMetadata('../test.txt'),
-  getCommmitActionListFrom,
-  getPrettyLog(title),
-  (prettyLog: string) => writeFileSync(`${config.saveTo}/md_test.md`, prettyLog)
-)();
+//git log --pretty="format:%h|%s|%b^@^"
+exec('git log --pretty="format:%h|%s|%b^@^', (err, stdout) => {
+  if (err) {
+    console.log(chalk.yellow('\nError Executing Command:\n'));
+    console.log(chalk.red(err.message));
+    return;
+  }
+  pipe(
+    () => getLogsMetadata(stdout),
+    getCommmitActionListFrom,
+    getPrettyLog(title),
+    (prettyLog: string) => writeFileSync(`${config.saveTo}/md_test.md`, prettyLog)
+  )();
+});
 
 
 
