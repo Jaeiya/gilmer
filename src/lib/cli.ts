6aeb5c64b5635fcab797720cb46fbd288f930f96
jaeiya <exanix@gmail.com>
2021-12-20 08:46:54 -0800

    feat(cli): better arg validation and error logs

--- src/lib/cli.ts
+++ src/lib/cli.ts
@@ -1,5 +1,11 @@
-import { color as c } from "./colors";
+import { Logger } from "./logger";
 
+
+
+const cc = Logger.console_colors;
+const log = Logger.lInfo;
+const logError = Logger.lErr;
+const logWarn  = Logger.lWarn;
 
 
 export namespace CLI {
@@ -25,63 +31,77 @@ export namespace CLI {
   export function processArgs() {
     const [,,fileName,...args] = process.argv;
 
-    if (fileName && fileName.includes('-')) {
-      console.log(c.r('\n\n ERROR:'), c.y('Invalid File Name'));
-      console.log(c.g('\n  NOTE:'),    c.d('Flags must be typed after the File Name\n\n'));
-      process.exit(1);
-    }
+    validateArgOrder(fileName);
+    validateFileName(fileName);
+
     state.filename  = fileName ?? state.filename;
     state.verbose   = args.includes('-v') || args.includes('-verbose');
     state.dateSince = getDateArg(args, ['-from', '-since']);
     state.dateUntil = getDateArg(args, ['-to',   '-until']);
   }
-
 }
 
 
+function validateArgOrder(filename: string|undefined) {
+  if (filename && filename[0] == '-') {
+    logError('error', cc.ywb('Invalid Argument Order'));
+    logWarn('info', cc.w('Flags must be typed'), cc.ywb('after'), cc.w('the'), cc.yw('file name'));
+    log('usage', cc.w('gilmer <filename> [<args>]'));
+    process.exit(1);
+  }
+}
+
+function validateFileName(filename: string|undefined) {
+  if (filename && !filename.match(/^[a-zA-Z0-9 ]+$/g)) {
+    logError('error', cc.ywb('Invalid File Name'));
+    logWarn('info',
+      cc.w('File names can only contain:'),
+      cc.yw('spaces, a-z, A-Z, and 0-9')
+    );
+    process.exit(1);
+  }
+}
+
 
 function getDateArg(args: string[], dateArgs: string[]) {
+  if (!args.length) return null;
   const dateArg = args.find(f => !!dateArgs.find(df => f.includes(df)));
   const argValue = (dateArg && getArgValue(dateArg)) || null;
-  return (argValue && getValidDate(argValue)) || null;
+  return (argValue && getValidDate(dateArg!.split('=')[0], argValue)) || null;
 }
 
 function getArgValue(arg: string) {
   if (!arg.includes('=')) {
-    logInvalidFlag(arg);
+    logInvalidArg(arg);
     process.exit(0);
   }
   return arg.split('=')[1];
 }
 
-function getValidDate(date: string) {
+function getValidDate(dateArg: string, date: string) {
   if ((new Date(date)).toString() == 'Invalid Date') {
-    logInvalidDate(date);
+    logInvalidDate(dateArg, date);
     process.exit(0);
   }
   return date;
 }
 
-function logInvalidFlag(flag: string) {
-  console.log(c.r('\n\n   ERROR:'), `${c.y('Malformed Flag')} ${c.r('(')}${c.w(flag)}${c.r(')')}`);
-  console.log(
-    c.g('\n    NOTE:'),
-    c.d(
-`Flags are set using an equals sign.
-        If a value has spaces, wrap it in quotes.`)
+function logInvalidArg(arg: string) {
+  logError('error', cc.yw('Malformed Argument'), cc.rd(`(${cc.wb(arg)})`));
+  logWarn('info',
+    cc.w('Args are assigned a value using an equals sign.'),
+    cc.w('\nIf a value has spaces, wrap it in quotes.')
   );
-  console.log(c.g('\n EXAMPLE:'), `${c.y('-')}from${c.y(`='`)}Aug 21, 2021${c.y(`'`)}\n\n`);
+  log('usage', cc.w(`gilmer <filename> ${arg}=<value>`));
 }
 
-function logInvalidDate(date: string) {
-  console.log(c.r('\n\n   ERROR:'), `${c.y('Invalid Date')} ${c.r('(')}${c.w(date)}${c.r(')')} `);
-  console.log(
-    c.g('\n    NOTE:',
-    c.d(
-`Make sure you entered the date properly.
-        If it contains spaces, wrap it in quotes.`)
-  ));
-  console.log(c.g('\n EXAMPLE:'), `${c.y('-')}from${c.y(`='`)}Aug 21, 2021${c.y(`'`)}\n\n`);
+function logInvalidDate(dateArg: string, date: string) {
+  logError('error', cc.yw('Invalid Date'), cc.rd(`(${cc.wb(date)})`));
+  logWarn('info',
+    cc.w('Make sure you entered a date compatible with the JavaScript Date Object.'),
+    cc.w('\nIf it contains spaces, wrap it in quotes.')
+  );
+  log('usage', cc.w(`gilmer <filename> [-${dateArg}=<value> or -${dateArg}='<value>']`));
 }
 
 
